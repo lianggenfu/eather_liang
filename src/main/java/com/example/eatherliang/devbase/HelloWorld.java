c3a675074ea496b96353446804cbfef02f800f4c
Genfu_liang <917055347@qq.com>
2021-03-17 00:03:26 +0800

    check new branches

---
+++ src/main/java/com/example/eatherliang/devbase/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.eatherliang.devbase;
+
+import org.junit.Test;
+
+public class HelloWorld {
+    @Test
+    public void hello(){
+        System.out.println("hello bytedance !");
+    }
+}
